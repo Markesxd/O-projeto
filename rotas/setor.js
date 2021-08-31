e68cc55901317a8c13c3a3efe7364a7dd290c2f0
Gustavo Castro <68030635+GustavoCastroo@users.noreply.github.com>
2021-08-31 12:43:11 -0300

    Update setor.js
    import { Router } from 'express';
    import SetorController from '../controller/setor.js';
    
    const setorRouter = Router();
    
    setorRouter.get('/', (req, res) => {
        SetorController.get()
            .then((resolve) => res.json(resolve))
            .catch((reject) => res.status(400).json(reject));
    });
    
    setorRouter.get('/:id', (req, res) => {
        SetorController.get()
        .then((resolve) => res.json(resolve))
        .catch((reject) => res.status(400).json(reject));
      });
    
    setorRouter.post('/', (req, res) => {
        SetorController.post(req.body)
            .then((resolve) => res.json(resolve))
            .catch((reject) => res.status(400).json(reject));
    });
    
    setorRouter.delete('/:id', (req, res) => {
        SetorController.delete(req.params.id)
        .then((resolve) => res.json(resolve))
        .catch((reject) => res.status(400).json(reject));
    });
      
    setorRouter.patch('/:id', (req, res) => {
        const params = {fields: req.query, id: req.params.id};
      
        SetorController.patch(params)
        .then((resolve) => res.json(resolve))
        .catch((reject) => res.status(400).json(reject));
    });
    
    export default setorRouter;

--- rotas/setor.js
+++ rotas/setor.js
@@ -1,13 +1,1 @@
-import {Router} from 'express';
 
-const setorRouter = Router();
-
-setorRouter.get('/', () => console.log('ba'));
-
-setorRouter.post('/', () => console.log('ba'));
-
-setorRouter.delete('/', () => console.log('ba'));
-
-setorRouter.patch('/', () => console.log('ba'));
-
-export default setorRouter;
