69edd85ffa3fd5239c9e23d24f37a363a978fe4e
Rafael <markesxd@gmail.com>
2021-11-07 08:46:00 -0300

    solving connection issues

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,32 @@
 import mysql from 'mysql2';
 
-const connection = mysql.connect({
+const config = process.env.CLEARDB_DATABASE_URL || {
   "host": process.env.DB_HOST,
   "port": process.env.DB_PORT,
   "database": process.env.DATABASE,
   "user": process.env.DB_USER,
   "password": process.env.DB_PASSWORD,
-})
+}
+
+const connection = mysql.createConnection(config);
+
+// let connection;
+
+// const handleDisconect = () => {
+//   connection = mysql.createConnection(config);
+  
+//   connection.connect((err) => {
+//     if(err){
+//       console.error(err);
+//       setTimeout(handleDisconect, 2000);
+//     }
+//   });
+
+// }
+
+// connection.on('error', (err) => {
+
+// })
+
 
 export default connection;
